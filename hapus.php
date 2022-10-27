8d3ad6768d3e5a38bd0e319a51294ef56bc84e37
PutriNuraini16 <92800945+PutriNuraini16@users.noreply.github.com>
2022-10-28 00:59:03 +0700

    Add files via upload

---
+++ hapus.php
@@ -0,0 +1,10 @@
+<?php 
+	if ($_GET['id_mahasiswa']){
+		 include "koneksi.php";
+		 $qry_hapus=mysqli_query($koneksi,"delete from mahasiswa where id_mahasiswa='".$_GET['id_mahasiswa']."'");
+		 if($qry_hapus){
+		 	echo "<script>alert('Sukses Hapus Data');location.href='datamahasiswa.php';</script>";
+		 } else {
+		 	echo "<script>alert('Gagal Hapus Data');location.href='datamahasiswa.php';</script>";
+		 }
+	}
---
+++ home.php
@@ -0,0 +1,15 @@
+<?php
+	include 'navbar.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+</head>
+<body>
+	<center><h1>Selamat Datang Di Perpustakaan Uniga</h1></center>
+
+</body>
+</html>
